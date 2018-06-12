8fad4e338ad8c6984eec7e252dbdc38ff0978817
tmfadhlul <tmfadhlul@gmail.com>
2018-06-13 01:52:09 +0700

    add albums, artist and track

--- app/library/actions.js
+++ app/library/actions.js
@@ -5,18 +5,38 @@ export function tabPlaylist(){
         type: 'TAB_PLAYLIST',
         payload: axios({
             method: 'GET',
-            url: 'https://f09670ea.ngrok.io/api/music'
+            url: `http://7598c4d0.ngrok.io/api/music`
         })
     }
 }
 
-// export function creatTabPlaylist(value){
-//     return {
-//         type: 'CREATE_TAB_PLAYLIST',
-//         payload: axios({
-//             method: 'POST',
-//             url: 'localhost:8000/api/music/',
-//             data: value
-//         })
-//     }
-// }
+export function tabArtist(){
+    return {
+        type: 'TAB_ARTIST',
+        payload: axios({
+            method: 'GET',
+            url: 'http://7598c4d0.ngrok.io/api/artist'
+        })
+    }
+}
+
+export function tabAlbums(){
+    return {
+        type: 'TAB_ALBUMS',
+        payload: axios({
+            method: 'GET',
+            url: 'http://7598c4d0.ngrok.io/api/albums'
+        })
+    }
+}
+
+export function creatTabPlaylist(value){
+    return {
+        type: 'CREATE_TAB_PLAYLIST',
+        payload: axios({
+            method: 'POST',
+            url: 'localhost:8000/api/music/',
+            data: value
+        })
+    }
+}
--- app/library/reducers.js
+++ app/library/reducers.js
@@ -1,10 +1,12 @@
 const initialState = {
-    results: [],
+    playlist: [],
+    artists: [],
+    albums: [],
     isLoading: false,
     isError: false,
     isSuccess: false
 }
-  
+
 const tabPlaylistReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'TAB_PLAYLIST_PENDING':
@@ -28,11 +30,58 @@ const tabPlaylistReducer = (state = initialState, action) => {
                 isLoading: false,
                 isError: true,
                 isSuccess: false,
-                results: action.payload.data
+                results: action.payload
             }
+        case 'TAB_ARTIST_PENDING':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false,
+                isSuccess: false
+            }
+        case 'TAB_ARTIST_FULFILLED':
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                isSuccess: true,
+                artists: action.payload.data
+            }
+        case 'TAB_ARTIST_REJECTED':
+            return {
+                ...state,
+                isLoading: false,
+                isError: true,
+                isSuccess: false,
+                artists: action.payload
+            }
+            case 'TAB_ALBUMS_PENDING':
+                return {
+                    ...state,
+                    isLoading: true,
+                    isError: false,
+                    isSuccess: false
+                }
+            case 'TAB_ALBUMS_FULFILLED':
+                return {
+                    ...state,
+                    isLoading: false,
+                    isError: false,
+                    isSuccess: true,
+                    albums: action.payload.data
+                }
+            case 'TAB_ALBUMS_REJECTED':
+                return {
+                    ...state,
+                    isLoading: false,
+                    isError: true,
+                    isSuccess: false,
+                    albums: action.payload
+                }
         default:
             return state
     }
+
 }
 
 export default tabPlaylistReducer
